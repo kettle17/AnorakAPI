3c69fddc39305d0b2460aa71f033d3332debfa78
kettle17 <trainee.callum.peters@sigmalabs.co.uk>
2025-08-25 16:26:07 +0100

    first commit

---
+++ src/main/java/org/example/anorakapi/AnorakApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.anorakapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnorakApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnorakApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/anorakapi/AnorakApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.anorakapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnorakApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
