215f770aae987fd412f2f3ed327cef43509aa606
kettle17 <trainee.callum.peters@sigmalabs.co.uk>
2025-08-26 01:12:10 +0100

    Jib/Docker ready

--- src/main/java/org/example/anorakapi/Sighting.java
+++ src/main/java/org/example/anorakapi/Sighting.java
@@ -21,20 +21,20 @@ public class Sighting {
     private String timestamp;
 
     Sighting(Station station, Train train, String timestamp) throws IllegalArgumentException {
-        if (station == null || train == null | timestamp == null) {
+        if (station == null || train == null || timestamp == null) {
             throw new IllegalArgumentException("Station, Train or Timestamp cannot be null");
         }
         if (timestamp.isEmpty()) {
             throw new IllegalArgumentException("Timestamp cannot be an empty string");
         }
         try {
-            OffsetDateTime.parse(timestamp); // validates as ISO 8601 with offset (e.g., 2025-08-25T17:35:42.123Z)
+            OffsetDateTime.parse(timestamp); // validates ISO 8601 with offset
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("Timestamp is an incorrect format");
+            throw new IllegalArgumentException("Timestamp is an incorrect format", e);
         }
         this.station = station;
         this.train = train;
-        this.timestamp = timestamp;
+        this.timestamp = timestamp; // still stored as string
     }
 
     public Sighting() {
